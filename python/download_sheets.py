1a86ef2d497f30ad1dc3a1653a93dafdc13183bf
Takeshi Kondo <ncukondo@gmail.com>
2022-04-09 02:33:15 +0000

    remove index from 2016/goalos.csv

--- python/download_sheets.py
+++ python/download_sheets.py
@@ -19,16 +19,15 @@ folder_ids=[
         '1FlCwwi71s1BI4uhOf2CefX85byj1Alo_', #各チーム編集用シート
 ]
 DIST_DIR=f"{BASE_DIR}/sheets/"
-info_file = f"{DIST_DIR}sheets_info.json"
 
-if os.path.exists(DIST_DIR) and os.path.exists(info_file):
+if os.path.exists(DIST_DIR):
     shutil.rmtree(DIST_DIR)
 
 os.makedirs(DIST_DIR,exist_ok=True)
 
 try:
     with tempfile.TemporaryDirectory() as temp_dir:
-        file_list=export_google_sheets(folder_ids,temp_dir,info_stor_file=f"{DIST_DIR}sheets_info.json")
+        file_list=export_google_sheets(folder_ids,temp_dir,info_store_file="")
         for file in file_list:
             book = pd.ExcelFile(file["export_path"])
             dir_name = os.path.join(DIST_DIR,file['name'])
@@ -36,7 +35,6 @@ try:
             for name in book.sheet_names:
                 sheet:pd.DataFrame = book.parse(name)
                 sheet.to_csv(os.path.join(dir_name,f"{name}.csv"),quoting=csv.QUOTE_NONNUMERIC,encoding="utf_8_sig",index=False,)
-        os.remove(info_file)
 except:
     raise
 
